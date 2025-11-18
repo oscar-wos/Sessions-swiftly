556a2bd9b917512d7179c0b88883e4cd1cdab303
oscar-wos <29248751+oscar-wos@users.noreply.github.com>
2025-11-18 15:11:55 +0000

    chore(cleanup): unused includes

--- RSession.Maps/Contracts/Core/IPlayerService.cs
+++ RSession.Maps/Contracts/Core/IPlayerService.cs
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 using RSession.Shared.Contracts;
-using SwiftlyS2.Shared.Players;
 
 namespace RSession.Maps.Contracts.Core;
 
