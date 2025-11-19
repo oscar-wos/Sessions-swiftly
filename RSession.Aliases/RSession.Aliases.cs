526fdec1b97427b5dcb7219914dc26f3100ce117
oscar-wos <29248751+oscar-wos@users.noreply.github.com>
2025-11-19 12:52:25 +0000

    fix(aliases): check correct service

--- RSession.Aliases/RSession.Aliases.cs
+++ RSession.Aliases/RSession.Aliases.cs
@@ -45,7 +45,7 @@ public sealed partial class Aliases(ISwiftlyCore core) : BasePlugin(core)
                 ?.Initialize(sessionEventService);
         }
 
-        if (interfaceManager.HasSharedInterface("RSession.EventService"))
+        if (interfaceManager.HasSharedInterface("RSession.PlayerService"))
         {
             ISessionPlayerService sessionPlayerService =
                 interfaceManager.GetSharedInterface<ISessionPlayerService>(
